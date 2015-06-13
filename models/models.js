a18e51576d127a031a43945c0f846d5b4be478ef
dcervera76 <dcervera76@gmail.com>
2015-06-13 13:48:31 +0200

    Multiples preguntas

--- models/models.js
+++ models/models.js
@@ -34,16 +34,15 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // succes() ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){
+  Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create(
-						{ pregunta: 'Capital de Italia',
-						  respuesta: 'Roma'
-				    })
-
-			.success(function(){console.log('Base de datos inicializada')});
+      Quiz.create({ pregunta: 'Capital de Italia',
+						        respuesta: 'Roma'});
+      Quiz.create({ pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa'})
+			.then(function(){console.log('Base de datos inicializada')});
           };
         });
       });
